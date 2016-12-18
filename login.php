1a34b69cdf6a35a1ee6c9be4ac6b975c0809f223
Tanya <tanyaiv94@mail.ua>
2016-12-18 18:36:50 +0200

    string for mobile

--- login.php
+++ login.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
-<!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
-<!--<meta name="viewport" content="width=1920">-->
-<!--<meta name="viewport" content="width=1920, initial-scale=1.0">-->
-<meta id="viewport" name="viewport" content="width=device-width, height=device-height, initial-scale=0.33, maximum-scale=1.2">
+    <meta id="viewport" name="viewport" content="width=device-width, height=device-height, initial-scale=0.33, maximum-scale=1.2">
 
     <title>IRB</title>
 
+    <link rel="alternate" href="http://m.irb.com.ua/" media="only screen and (max-width: 640px)">
+    
     <meta name="keywords" content=""/>
     <meta name="description" content=""/>
 
